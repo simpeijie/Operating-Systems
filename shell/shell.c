fc3dc50736ef91ddc406827d76150b43e18c0ac4
Pei Jie Sim <simpeijie@gmail.com>
2017-08-02 10:29:07 -0700

    Remove DS Store

--- shell/shell.c
+++ shell/shell.c
@@ -153,99 +153,99 @@ int main(unused int argc, unused char *argv[]) {
 
     /* Find which built-in function to run. */
     int fundex = lookup(tokens_get_token(tokens, 0));
-		size_t args_size = 0;
-		char *arg;
-		char **args = (char **) malloc(sizeof(char *));
- 	  char *tok;
-		char path[1024];
-		char *PATH = getenv("PATH");
-		char *filename;
-		int to_file = -1;  
-		bool background = false;
-		int i = 0;
+	size_t args_size = 0;
+	char *arg;
+	char **args = (char **) malloc(sizeof(char *));
+	char *tok;
+	char path[1024];
+	char *PATH = getenv("PATH");
+	char *filename;
+	int to_file = -1å;  
+	bool background = false;
+	int i = 0;
 
-		if (fundex >= 0) {
+	if (fundex >= 0) {
       cmd_table[fundex].fun(tokens);
     } else {
       /* REPLACE this to run commands as programs. */
 			
-			// get the program path and arguments needed to run the program
-			// and store them in ARGS, i.e. ARGS = <prog_name> <arg1> <arg2> ...
-			while ((arg = tokens_get_token(tokens, i)) != NULL) {
-				if (*arg == '>') {
-					filename = tokens_get_token(tokens, ++i);
-					to_file = 1;
-				}
-				else if (*arg == '<') {
-					filename = tokens_get_token(tokens, ++i);
-					to_file = 0;
-				}
-				else if (*arg == '&'){
-					background = true;
-					i++;
-				}
-				else {
-					append(&args, &i, arg);
-					args_size = i;
-				}
+		// get the program path and arguments needed to run the program
+		// and store them in ARGS, i.e. ARGS = <prog_name> <arg1> <arg2> ...
+		while ((arg = tokens_get_token(tokens, i)) != NULL) {
+			if (*arg == '>') {
+				filename = tokens_get_token(tokens, ++i);
+				to_file = 1;
 			}
-			
-			int new_fd;
-			if (filename && to_file != -1)
-				new_fd = get_file_des(filename, to_file);
-
-			args = (char **) realloc(args, sizeof(char *) * (args_size+1));
-			args[args_size] = NULL;
-			args_size += 1;
-			
-			char *is_full = strrchr(args[0], '/');
-
-			if (is_full == NULL) {
-				tok = strtok(PATH, ":");
-				while (tok != NULL) {
-					strcpy(path, tok);
-					strcat(path, "/");
-					strcat(path, args[0]); 
-					if (access(path, F_OK) != -1) {
-						args[0] = path;
-						break;
-					}
-					tok = strtok(NULL, ":");
-				}
+			else if (*arg == '<') {
+				filename = tokens_get_token(tokens, ++i);
+				to_file = 0;
 			}
-		
-			pid_t pid;
-			int status;
-		
-			if ((pid = fork()) < 0) {
-				perror("fork() error.");
-				exit(1);
-			}
-
-			if (pid == 0) {
-				signal(SIGINT, SIG_DFL);
-				signal(SIGTTIN, SIG_IGN);
-				signal(SIGTTOU, SIG_IGN);
-				if (!background) {
-					tcsetpgrp(0, getpgid(0));
-				}
-				if (to_file == 1)
-					dup2(new_fd, 1);
-				else if (to_file == 0)
-					dup2(new_fd, 0);
-				execv(args[0], args);
+			else if (*arg == '&'){
+				background = true;
+				i++;
 			}
 			else {
-				signal(SIGINT, SIG_IGN);
-				signal(SIGTTIN, SIG_IGN);
-				signal(SIGTTOU, SIG_IGN);
-				setpgid(pid, pid);
-				if (!background) {
-					waitpid(pid, &status, 0);
-					tcsetpgrp(0, shell_pgid);
-				}
-			}          	
+				append(&args, &i, arg);
+				args_size = i;
+			}
 		}
+		
+		int new_fd;
+		if (filename && to_file != -1)
+			new_fd = get_file_des(filename, to_file);
+
+		args = (char **) realloc(args, sizeof(char *) * (args_size+1));
+		args[args_size] = NULL;
+		args_size += 1;
+		
+		char *is_full = strrchr(args[0], '/');
+
+		if (is_full == NULL) {
+			tok = strtok(PATH, ":");
+			while (tok != NULL) {
+				strcpy(path, tok);
+				strcat(path, "/");
+				strcat(path, args[0]); 
+				if (access(path, F_OK) != -1) {
+					args[0] = path;
+					break;
+				}
+				tok = strtok(NULL, ":");
+			}
+		}
+	
+		pid_t pid;
+		int status;
+	
+		if ((pid = fork()) < 0) {
+			perror("fork() error.");
+			exit(1);
+		}
+
+		if (pid == 0) {
+			signal(SIGINT, SIG_DFL);
+			signal(SIGTTIN, SIG_IGN);
+			signal(SIGTTOU, SIG_IGN);
+			if (!background) {
+				tcsetpgrp(0, getpgid(0));
+			}
+			if (to_file == 1)
+				dup2(new_fd, 1);
+			else if (to_file == 0)
+				dup2(new_fd, 0);
+			execv(args[0], args);
+		}
+		else {
+			signal(SIGINT, SIG_IGN);
+			signal(SIGTTIN, SIG_IGN);
+			signal(SIGTTOU, SIG_IGN);
+			setpgid(pid, pid);
+			if (!background) {
+				waitpid(pid, &status, 0);
+				tcsetpgrp(0, shell_pgid);
+			}
+		}          	
+	}
 		
     if (shell_is_interactive)
       /* Please only print shell prompts when standard input is not a tty */
